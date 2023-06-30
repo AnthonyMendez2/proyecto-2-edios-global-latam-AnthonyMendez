8ded5c7bd3c43664dbcfaef8cd801460a905de8a
Anthony <soyanthonymendez@gmail.com>
2023-06-30 01:32:10 -0300

    final para entrega del proyecto2

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-document.write('<hr>', "JavaScript");
+
+const menuItems = document.querySelectorAll(".menu-item");
+
+menuItems.forEach(function(item) {
+    item.addEventListener ("click", function(e) {
+        const currentItem = document.querySelector(".active");
+        if (currentItem){currentItem.classList.remove("active");}
+        e.target.classList.add("active");
+    })
+});
